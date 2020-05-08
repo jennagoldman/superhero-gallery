f50d64362cae8ab425c9c9a0af536a6f13606d88
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-08 16:42:40 -0700

    add render method to ThemeProvider and update App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route 
+} from 'react-router-dom';
+import ThemeProvider from '../../hooks/ThemeProvider.jsx';
 import Gallery from '../../containers/Gallery/Gallery.jsx';
 
 export default function App() {
   return (
     <>
-      <Gallery />
+      <ThemeProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Gallery} />
+          </Switch>
+        </Router>
+      </ThemeProvider>
     </>
   );
 }
