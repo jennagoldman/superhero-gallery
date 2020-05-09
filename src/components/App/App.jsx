aa7dbc3f452902517a1ba7039603c545ead97fc2
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-09 10:49:27 -0700

    remove unneeded React Router components from App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,17 +5,20 @@ import {
   Route 
 } from 'react-router-dom';
 import ThemeProvider from '../../hooks/ThemeProvider.jsx';
+import HeroesProvider from '../../hooks/HeroesProvider.jsx';
 import Gallery from '../../containers/Gallery/Gallery.jsx';
 
 export default function App() {
   return (
     <>
       <ThemeProvider>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Gallery} />
-          </Switch>
-        </Router>
+        <HeroesProvider>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Gallery} />
+            </Switch>
+          </Router>
+        </HeroesProvider>
       </ThemeProvider>
     </>
   );
