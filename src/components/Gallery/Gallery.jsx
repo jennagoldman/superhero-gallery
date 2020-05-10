affeb98e1b01131a00f150f967332ef155a1662b
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-10 16:34:14 -0700

    change CSS based on toggle checked

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -3,10 +3,10 @@ import HeroesList from './HeroesList.jsx';
 import Paging from './Paging.jsx';
 
 const Gallery = () => (
-  <>
+  <main>
     <Paging />
     <HeroesList />
-  </>
+  </main>
 );
 
 export default Gallery;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { useToggle } from '../../hooks/ThemeProvider';
+import { useTheme, useToggle } from '../../hooks/ThemeProvider';
+import styles from './Header.css';
 
 const Header = () => {
   const toggle = useToggle();
+  const theme = useTheme();
 
   return (
-    <header>
+    <header className={`${styles[theme]}`}>
       <h1>Marvel Superheroes Gallery</h1>
       <input type="checkbox" onChange={toggle} id="toggle" />
       <label htmlFor="toggle">Toggle Dark Mode</label>
