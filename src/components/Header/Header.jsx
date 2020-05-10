31362b0364219c58e1e4dc9516e17763415a8175
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-10 14:47:32 -0700

    add checkbox for toggle to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useToggle } from '../../hooks/ThemeProvider';
 
-const Header = () => (
-  <header>
-    <h1>Marvel Superheroes Gallery</h1>
-  </header>
-);
+const Header = () => {
+  const toggle = useToggle();
+
+  return (
+    <header>
+      <h1>Marvel Superheroes Gallery</h1>
+      <input type="checkbox" onChange={toggle} id="toggle" />
+      <label htmlFor="toggle">Toggle Dark Mode</label>
+    </header>
+  ); 
+};
 
 export default Header;
