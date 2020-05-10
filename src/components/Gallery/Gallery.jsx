1b656c99ab550bb86b40b4c02bd811f1f6706e6d
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-10 16:53:37 -0700

    Header and main styling, add Google Fonts link

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useTheme } from '../../hooks/ThemeProvider.jsx';
 import HeroesList from './HeroesList.jsx';
 import Paging from './Paging.jsx';
+import styles from './Gallery.css';
 
-const Gallery = () => (
-  <main>
-    <Paging />
-    <HeroesList />
-  </main>
-);
+const Gallery = () => {
+  const theme = useTheme();
+
+  return (
+    <main className={`${styles[theme]}`}>
+      <Paging />
+      <HeroesList />
+    </main>
+  );
+};
 
 export default Gallery;
